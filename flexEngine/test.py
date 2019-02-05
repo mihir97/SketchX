c67966df75f6d6e093dd8204ebb05eaeb4f62a48
Ameya Apte <ameyaapte1@gmail.com>
2019-02-05 19:23:14 +0530

    Added region finder script

--- flexEngine/test.py
+++ flexEngine/test.py
@@ -10,33 +10,38 @@ def generate_html(data,css):
             with tag('style'):
                 doc.asis(css)
         with tag('body'):
-            with tag('h1'):
-                text('Hello world!')
+            for d in data:
+                doc.asis(d)
 
     result = indent(doc.getvalue())
     print(result)
 
 def generate_css(name,data):
     output_str = []
-    output_str.append(name)
-    output_str.apend(' {\n')
+    output_str.append("\n"+name)
+    output_str.append(' {\n')
     for key in data:
         output_str.append("\t"+key+": "+data[key]+";\n")
     output_str.append("}\n")
     return ''.join(output_str)
 
 content = []
+css_list = []
 xmax = data["xmax"]
 ymax = data["ymax"]
 
-doc, tag, text = Doc().tagtext()
 #TODO: Sort the regions in a top down fashion
 for element in data['regions']:
+    doc, tag, text = Doc().tagtext()
     if element["type"] == "Label":
         with tag('label'):
             text(element["content"])
-            content.append(doc.getvalue())
+        content.append(doc.getvalue())
+    elif element["type"] == "TextBox":
+        doc.stag('input', type = 'text', value = element['content'])
+        content.append(doc.getvalue())
 
-generate_html(content,"")
+css_list.append( generate_css("body",{"display":"flex","flex-direction":"column"}))
+generate_html(content,'\n'.join(css_list))
 
 
