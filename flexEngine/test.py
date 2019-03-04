071ff5f8ffc691aab13a228ef9f868540d127cff
VaradGhodake <varadghodake@gmail.com>
2019-03-04 20:35:13 +0530

    Changes in flexengine

--- flexEngine/test.py
+++ flexEngine/test.py
@@ -15,6 +15,17 @@ def generate_html(data,css):
 
     result = indent(doc.getvalue())
     print(result)
+    with open('test.html', 'w') as HTMLCode:
+        HTMLCode.write(result)
+
+def generate_css_util():
+    # for element in data['regions']:
+        # generate_css(element["type"], {CSS Styles })
+        # Calculations will go here
+        # Location, Height, Width and other stuff
+        # Sibling and parent relations based on len(tag(region))
+        # will provide further details about the flex columns or row 
+        pass
 
 def generate_css(name,data):
     output_str = []
@@ -37,11 +48,24 @@ for element in data['regions']:
         with tag('label'):
             text(element["content"])
         content.append(doc.getvalue())
+        content.append("<br/>")
     elif element["type"] == "TextBox":
-        doc.stag('input', type = 'text', value = element['content'])
+        doc.stag('input', type = 'text', placeholder = element['content'], id = element['id'])
+        content.append(doc.getvalue())
+        content.append("<br/>")
+    elif element["type"] == "CheckBox":
+        with tag('input', type = 'checkbox', value = element['content'], id = element['id']):
+            text(element["content"])
+        content.append(doc.getvalue())
+        content.append("<br/>")
+    elif element["type"] == "Button":
+        doc.stag('input', type = 'button', value = element['content'], id = element['id'])
+        content.append(doc.getvalue())
+    elif element["type"] == "Image":
+        doc.stag('img', src=element['src'], height=element['height'], width= element['width'])
         content.append(doc.getvalue())
 
-css_list.append( generate_css("body",{"display":"flex","flex-direction":"column"}))
+css_list.append( generate_css("body",{"display":"flex","flex-direction":"column", 
+"margin" : "auto", "margin-top": "15px" ,"width": "60%"}))
+# css_list.append(generate_css_util())
 generate_html(content,'\n'.join(css_list))
-
-
